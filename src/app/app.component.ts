052a3a98d8138525a5732ee7f5729093519442b8
ErosJBC <ebazanc@uni.pe>
2023-06-08 20:21:13 -0500

    Avanzando con diseño de página web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,15 @@ import * as $ from 'jquery';
 })
 export class AppComponent implements OnInit {
 	title = 'ssoldig-web';
+	showPage = false;
 
 	ngOnInit(): void {
 		$(window).on('load', () => {
 			const startLoading = `
-				<div id="start-loading" class="fixed bg-white top-0 bottom-0 right-0 left-0 z-30 flex items-center justify-center">
-					<img src="../../../../assets/img/SSD-gif.gif" alt="Logo de SSOLDIG">
+				<div id="start-loading" class="fixed bg-white top-0 bottom-0 right-0 left-0 z-50 flex items-center justify-center">
+					<div class="aspect-video">
+						<img src="../../../../assets/img/SSD-gif.gif" alt="Logo de SSOLDIG">
+					</div>	
 				</div>
 			`;
 
@@ -21,7 +24,9 @@ export class AppComponent implements OnInit {
 
 			setTimeout(() => {
 				$('#start-loading').fadeOut('slow');
-			}, 5500);
-		})
+				this.showPage = true;
+			}, 6000);
+
+		});
 	}
 }
--- src/app/views/home/presentation/presentation.component.ts
+++ src/app/views/home/presentation/presentation.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-presentation',
-  templateUrl: './presentation.component.html',
-  styleUrls: ['./presentation.component.scss']
+	selector: 'app-presentation',
+	templateUrl: './presentation.component.html',
+	styleUrls: ['./presentation.component.scss']
 })
 export class PresentationComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		const text = document.getElementById('writing');
+		this.writingEffect('¡Un mundo digital en tus manos!', 200, text);
+	}
+
+	writingEffect = (text: string = '', time: number = 200, element: any = '') => {
+		let characters = text.split('');
+		element.innerHTML = ''
+		let index = 0;
+		let lengthText = text.length;
+		let write = setInterval(function () {
+			if (index === characters.length) {
+				element.innerHTML = text.substring(0, lengthText);
+				lengthText--;
+				if (lengthText === 0) {
+					element.innerHTML = '';
+					index = 0;
+					lengthText = text.length;
+				}
+			} else {
+				element.innerHTML += characters[index];
+				index++;
+			}
+		}, time);
+	}
 
 }
