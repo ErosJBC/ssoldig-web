05c4da5e9aaba59f3d2ff963feae3a87be2f16cb
ErosJBC <ebazanc@uni.pe>
2023-06-13 12:34:40 -0500

    Agregando animación a imágenes de la carga principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,15 @@ import * as $ from 'jquery';
 export class AppComponent implements OnInit {
 	title = 'ssoldig-web';
 	showPage = false;
-	time_base = 2500;
+	time_base = 3250;
 
 	ngOnInit(): void {
 		const startLoading = `
 			<div id="start-loading" class="fixed bg-white top-0 bottom-0 right-0 left-0 z-50 flex items-center justify-center">
 				<div id="images" class="grid grid-cols-3 mx-auto">
-					<img src="../assets/img/ssd-orange-logo.png" alt="Logo de SSOLDIG">
-					<img src="../assets/img/ssd-skyblue-logo.png" alt="Logo de SSOLDIG">
-					<img src="../assets/img/ssd-purple-logo.png" alt="Logo de SSOLDIG">
+					<img class="animate__animated animate__fadeInLeft animate__slow" src="../assets/img/ssd-orange-logo.png" alt="Logo de SSOLDIG">
+					<img class="animate__animated animate__fadeInDown animate__slow" src="../assets/img/ssd-skyblue-logo.png" alt="Logo de SSOLDIG">
+					<img class="animate__animated animate__fadeInRight animate__slow" src="../assets/img/ssd-purple-logo.png" alt="Logo de SSOLDIG">
 				</div>
 			</div>
 		`;
@@ -43,7 +43,6 @@ export class AppComponent implements OnInit {
 				$('#start-loading').fadeOut('slow');
 				this.showPage = true;
 			}, this.time_base + 7000);
-
 		});
 	}
 }
